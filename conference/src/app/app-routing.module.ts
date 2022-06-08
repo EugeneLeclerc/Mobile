617e5a6929622455716539b9bd220d6d96d3c37e
Eugene <mail@eugeneleclerc.Fr>
2022-06-08 11:26:34 +0200

    Menu

--- conference/src/app/app-routing.module.ts
+++ conference/src/app/app-routing.module.ts
@@ -5,34 +5,40 @@ const routes: Routes = [
   {
     path: '',
     redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
-  },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    pathMatch: 'full',
   },
   {
     path: 'session-list',
-    loadChildren: () => import('./session-list/session-list.module').then( m => m.SessionListPageModule)
+    loadChildren: () =>
+      import('./session-list/session-list.module').then(
+        (m) => m.SessionListPageModule
+      ),
   },
   {
     path: 'session-detail',
-    loadChildren: () => import('./session-detail/session-detail.module').then( m => m.SessionDetailPageModule)
+    loadChildren: () =>
+      import('./session-detail/session-detail.module').then(
+        (m) => m.SessionDetailPageModule
+      ),
   },
   {
     path: 'notes',
-    loadChildren: () => import('./notes/notes.module').then( m => m.NotesPageModule)
+    loadChildren: () =>
+      import('./notes/notes.module').then((m) => m.NotesPageModule),
   },
   {
     path: 'phone-details',
-    loadChildren: () => import('./phone-details/phone-details.module').then( m => m.PhoneDetailsPageModule)
-  }
+    loadChildren: () =>
+      import('./phone-details/phone-details.module').then(
+        (m) => m.PhoneDetailsPageModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
