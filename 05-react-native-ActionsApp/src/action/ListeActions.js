7bf41b3554b8ddf3c9f841a61e139a2cb6c0c8d1
Eugène Leclerc <mail@eugeneleclerc.fr>
2022-06-08 16:16:14 +0200

    First

---
+++ 05-react-native-ActionsApp/src/action/ListeActions.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+import {UneAction} from './UneAction'
+
+
+export const ListeActions = () => {
+
+    return (
+        <View>
+            <Text>Ici prochainement une liste d'actions</Text>
+        </View>
+    )
+}
